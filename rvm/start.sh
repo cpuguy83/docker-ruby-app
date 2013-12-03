2f2c91990cbbf1b4298cef6f3818565ea6c273a1
Brian Goff <cpuguy83@gmail.com>
2013-12-03 11:42:42 -0500

    Add RVM files

---
+++ rvm/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /usr/local/rvm/scripts/rvm
+rvm use $RUBY-$RUBY_VERSION
+if [[ ! -d $APP_DIR ]]; then
+        if [[ ! -z $DEPLOY_KEY ]]; then
+                printf %"s\n" "${DEPLOY_KEY}" > /.id_rsa
+                chmod 600 /.id_rsa
+                env --unset=DEPLOY_KEY
+                mkdir ~/.ssh
+                echo "  IdentityFile    /.id_rsa" >> /etc/ssh/ssh_config
+                echo "  StrictHostKeyChecking   no" >> /etc/ssh/ssh_config
+        fi
+        git clone $APP_REPO $APP_DIR && \
+        cd $APP_DIR && \
+        rvm ${RUBY}-${RUBY_VERSION} do $INSTALL_CMD
+fi
+rvm ${RUBY}-${RUBY_VERSION} do $START_CMD
